31e25141dcc36394c08e5059f76bda96347113dc
Rafael Guerra <rafael@eregionforge.com>
2020-05-16 14:14:19 -0300

    Deploying our API to a server on AWS part I

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd1our!+(6h&c5loxd^^k(^tm5_75d&2_j(d3n@cf%upouacrwo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profile_api.UserProfile'
+
+STATIC_ROOT = 'static/'
